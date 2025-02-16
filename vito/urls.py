a90024b3170dfa068c6ed39aaf618ba35dd3f8f7
vito <ac1246897@gmail.com>
2025-02-16 13:41:37 +0800

    set up entrypoint to load build in static file in container

--- vito/urls.py
+++ vito/urls.py
@@ -1,5 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -13,3 +15,6 @@ urlpatterns = [
    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
