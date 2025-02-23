1bf47c52b8d316a5660e56b8357ecfb408a93427
vito <ac1246897@gmail.com>
2025-02-23 09:46:38 +0800

    specifically design serializer and view for transaction (crud)

--- bookkeeping/serializers.py
+++ bookkeeping/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Asset, Account, Transaction
+from django.db import transaction
 
 class AccountSerializer(serializers.ModelSerializer):    
     class Meta:
@@ -16,7 +17,7 @@ class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = [
-            'account', 'asset', 'transaction_type', 'from_account', 'to_account', 'amount'
+            'account', 'asset', 'transaction_type', 'amount', 'from_account', 'to_account'
         ]
 
     def validate(self, data):
@@ -24,34 +25,100 @@ class TransactionSerializer(serializers.ModelSerializer):
         from_account = data.get('from_account')
         to_account = data.get('to_account')
         asset = data.get('asset')
+        amount = data.get('amount')
 
         if transaction_type == Transaction.TransactionType.TRANSFER:
             if not from_account or not to_account:
                 raise serializers.ValidationError("Both from_account and to_account are required for a Transfer transaction.")
             if from_account == to_account:
                 raise serializers.ValidationError("from_account and to_account cannot be the same account.")
+            if from_account.balance < amount:
+                raise serializers.ValidationError("Insufficient balance in from_account.")
 
-        elif transaction_type in [Transaction.TransactionType.INCOME, Transaction.TransactionType.EXPENSE]:
+
+        elif transaction_type == Transaction.TransactionType.EXPENSE:
             if from_account or to_account:
                 raise serializers.ValidationError("from_account and to_account should only be set for Transfer transactions.")
-        # Validate Asset existence
-        if asset and not Asset.objects.filter(id=asset.id).exists():
-            raise serializers.ValidationError("The specified asset does not exist.")
+            if data['account'].balance < amount:
+                raise serializers.ValidationError("Insufficient balance for expense.")
+
+        elif transaction_type == Transaction.TransactionType.INCOME:
+            if from_account or to_account:
+                raise serializers.ValidationError("from_account and to_account should only be set for Transfer transactions.")
 
         return data
 
     def create(self, validated_data):
-        account = validated_data.get('account')
-        asset = validated_data.get('asset')
-        asset_type = self.initial_data.get('asset_type')
-        
-        if not Account.objects.filter(username=account.username).exists():
+        with transaction.atomic():
+            account = validated_data['account']
+            amount = validated_data['amount']
+            transaction_type = validated_data['transaction_type']
+            from_account = validated_data.get('from_account')
+            to_account = validated_data.get('to_account')
+
             account, _ = Account.objects.get_or_create(username=account.username, defaults={"balance": 0})
 
-        if asset is None and asset_type:
-            asset, _ = Asset.objects.get_or_create(account=account, asset_type=asset_type, defaults={"balance": 0})
 
-        validated_data['account'] = account
-        validated_data['asset'] = asset
+            # update balance
+            if transaction_type == Transaction.TransactionType.INCOME:
+                account.balance += amount
 
-        return super().create(validated_data)
+            elif transaction_type == Transaction.TransactionType.EXPENSE:
+                account.balance -= amount
+
+            elif transaction_type == Transaction.TransactionType.TRANSFER:
+                from_account.balance -= amount
+                to_account.balance += amount
+                from_account.save()
+                to_account.save()
+
+            account.save()
+
+            return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        with transaction.atomic():
+            # restore the influence of old transaction first
+            if instance.transaction_type == Transaction.TransactionType.INCOME:
+                instance.account.balance -= instance.amount
+            elif instance.transaction_type == Transaction.TransactionType.EXPENSE:
+                instance.account.balance += instance.amount
+            elif instance.transaction_type == Transaction.TransactionType.TRANSFER:
+                instance.from_account.balance += instance.amount
+                instance.to_account.balance -= instance.amount
+
+            instance.account.save()
+            if instance.transaction_type == Transaction.TransactionType.TRANSFER:
+                instance.from_account.save()
+                instance.to_account.save()
+                
+            for attr, value in validated_data.items():
+                setattr(instance, attr, value)
+                
+            if instance.transaction_type == Transaction.TransactionType.INCOME:
+                instance.account.balance += instance.amount
+            elif instance.transaction_type == Transaction.TransactionType.EXPENSE:
+                instance.account.balance -= instance.amount
+            elif instance.transaction_type == Transaction.TransactionType.TRANSFER:
+                instance.from_account.balance -= instance.amount
+                instance.to_account.balance += instance.amount
+
+            instance.save()
+            return instance
+
+    def delete(self, instance):
+        with transaction.atomic():
+            if instance.transaction_type == Transaction.TransactionType.INCOME:
+                instance.account.balance -= instance.amount
+            elif instance.transaction_type == Transaction.TransactionType.EXPENSE:
+                instance.account.balance += instance.amount
+            elif instance.transaction_type == Transaction.TransactionType.TRANSFER:
+                instance.from_account.balance += instance.amount
+                instance.to_account.balance -= instance.amount
+
+            instance.account.save()
+            if instance.transaction_type == Transaction.TransactionType.TRANSFER:
+                instance.from_account.save()
+                instance.to_account.save()
+
+            super(Transaction, instance).delete()
--- bookkeeping/views.py
+++ bookkeeping/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.db import transaction as db_transaction
 from .serializers import AccountSerializer, AssetSerializer, TransactionSerializer
 from .models import Account, Asset, Transaction
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 class AccountViewSet(viewsets.ModelViewSet):
@@ -18,12 +19,33 @@ class TransactionViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         data = request.data
-
-        # 確保金額大於 0
-        if data.get("amount") and float(data["amount"]) <= 0:
+        if float(data.get("amount", 0)) <= 0:
             return Response({"error": "交易金額必須大於 0"}, status=status.HTTP_400_BAD_REQUEST)
 
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+
+        with db_transaction.atomic():
+            self.perform_create(serializer)
+        
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        transaction_instance = self.get_object()
+        data = request.data
+
+        serializer = self.get_serializer(transaction_instance, data=data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        with db_transaction.atomic():
+            serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        transaction_instance = self.get_object()
+
+        with db_transaction.atomic():
+            super().delete(instance)
+
+        return Response({"message": "Transaction deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
