5f8761fffcb260832affe50191824aedbbe1d2fd
9-8-7-6 <ac1246897@gmail.com>
2025-03-04 06:00:01 +0800

    modify the migrations file

--- migrations/20250303215636_create_accounts_table.up.sql
+++ migrations/20250303215636_create_accounts_table.up.sql
@@ -1,5 +1,5 @@
--- Add migration script here
-CREATE TABLE accounts (
+-- Add up migration script here
+CREATE TABLE IF NOT EXISTS accounts (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL,
     balance DOUBLE PRECISION NOT NULL,
