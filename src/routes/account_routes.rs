421e7b18224440aa4132db8f1ef9040e7bf2f6cf
9-8-7-6 <ac1246897@gmail.com>
2025-03-04 05:42:34 +0800

    modify the url syntax

--- src/routes/account_routes.rs
+++ src/routes/account_routes.rs
@@ -9,9 +9,9 @@ use std::sync::Arc;
 pub fn account_routes(state: Arc<PgPool>) -> Router {
     Router::new()
         .route("/accounts", get(get_all_accounts))
-        .route("/accounts/:id", get(get_account))
+        .route("/accounts/{id}", get(get_account))
         .route("/accounts", post(add_account))
-        .route("/accounts/:id", put(update_account))
-        .route("/accounts/:id", delete(delete_account_handler))
+        .route("/accounts/{id}", put(update_account))
+        .route("/accounts/{id}", delete(delete_account_handler))
         .with_state(state)
 }
