e8f6b96d3a3cbf20cad6d3c470a7d9d1f4041869
9-8-7-6 <ac1246897@gmail.com>
2025-03-08 23:41:34 +0800

    init the axum-login but stall not works

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,7 +1,9 @@
+use async_trait::async_trait;
 use axum_login::{AuthUser, AuthnBackend, UserId};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
+use std::collections::HashMap;
 use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
@@ -25,5 +27,34 @@ impl AuthUser for User {
 
     fn session_auth_hash(&self) -> &[u8] {
         self.id.as_bytes()
+    }
+}
+
+#[derive(Clone, Default)]
+struct Backend {
+    users: HashMap<Uuid, User>,
+}
+
+#[derive(Clone)]
+struct Credentials {
+    user_id: Uuid,
+}
+
+#[async_trait]
+impl AuthnBackend for Backend {
+    type User = User;
+    type Credentials = Credentials;
+    type Error = std::convert::Infallible;
+
+    
+    async fn authenticate(
+        &self,
+        Credentials { user_id }: Self::Credentials,
+    ) -> Result<Option<Self::User>, Self::Error> {
+        Ok(self.users.get(&user_id).cloned())
+    }
+
+    async fn get_user(&self, user_id: &UserId<Self>) -> Result<Option<Self::User>, Self::Error> {
+        Ok(self.users.get(user_id).cloned())
     }
 }
