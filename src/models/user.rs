985c425560ceae231171af635cc6d68ad2c45a84
9-8-7-6 <ac1246897@gmail.com>
2025-03-08 23:10:45 +0800

    impl AuthUser for user

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,9 +1,10 @@
+use axum_login::{AuthUser, AuthnBackend, UserId};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 use uuid::Uuid;
 
-#[derive(Debug, Serialize, Deserialize, FromRow)]
+#[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
 pub struct User {
     pub id: Uuid,
     pub username: String,
@@ -13,4 +14,16 @@ pub struct User {
     pub is_staff: bool,
     pub is_active: bool,
     pub date_joined: DateTime<Utc>,
+}
+
+impl AuthUser for User {
+    type Id = Uuid;
+
+    fn id(&self) -> Self::Id {
+        self.id
+    }
+
+    fn session_auth_hash(&self) -> &[u8] {
+        self.id.as_bytes()
+    }
 }
