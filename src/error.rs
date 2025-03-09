53c5d57775127999c7b08a3765a732a2539feda9
9-8-7-6 <ac1246897@gmail.com>
2025-03-10 06:28:43 +0800

    create a crate to deal with error response

---
+++ src/error.rs
@@ -0,0 +1,27 @@
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    LoginFail,
+    DatabaseError,
+    InvalidPayload,
+    InternalServerError,
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        match self {
+            Error::LoginFail => (StatusCode::UNAUTHORIZED, "LOGIN_FAILED").into_response(),
+            Error::DatabaseError => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "DATABASE_ERROR").into_response()
+            }
+            Error::InvalidPayload => (StatusCode::BAD_REQUEST, "INVALID_PAYLOAD").into_response(),
+            Error::InternalServerError => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR").into_response()
+            }
+        }
+    }
+}
