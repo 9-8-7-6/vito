055c25e21f070d5e2780f629e9f75201596e3d03
9-8-7-6 <ac1246897@gmail.com>
2025-03-10 11:52:50 +0800

    modify REDIS_URL because Rust run on local instead of container

--- src/routes/login_logout_routes.rs
+++ src/routes/login_logout_routes.rs
@@ -6,5 +6,6 @@ use std::sync::Arc;
 pub fn login_routes(state: Arc<PgPool>) -> Router {
     Router::new()
         .route("/api/login", post(api_login))
+        .route("/api/logout", post(api_logout))
         .with_state(state)
 }
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,7 @@
 pub mod account_routes;
 pub mod asset_routes;
 pub mod category_routes;
-pub mod login_routes;
+pub mod login_logout_routes;
 pub mod recurring_transaction_routes;
 pub mod transaction_routes;
 pub mod user_routes;
