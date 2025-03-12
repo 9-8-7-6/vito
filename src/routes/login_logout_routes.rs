ef721b0207159c58186da5446baf68f63ed680e4
9-8-7-6 <ac1246897@gmail.com>
2025-03-12 16:16:51 +0800

    move register route backward because it doesn't need to login

--- src/routes/login_logout_routes.rs
+++ src/routes/login_logout_routes.rs
@@ -8,8 +8,8 @@ pub fn login_routes(backend: Backend) -> Router {
     Router::new()
         .route("/api/login", post(api_login))
         .route("/api/logout", post(api_logout))
-        .route("/api/register", post(api_register))
         .route("/api/delete_account", post(api_delete_account))
         .route_layer(login_required!(Backend, login_url = "/login"))
+        .route("/api/register", post(api_register))
         .with_state(backend)
 }
