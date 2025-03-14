ea27947d5f5544d25c83c38136c76737e9ba3dca
9-8-7-6 <ac1246897@gmail.com>
2025-03-14 12:16:35 +0800

    comment login_required auth layer bacause the function is to do now

--- src/routes/login_logout_routes.rs
+++ src/routes/login_logout_routes.rs
@@ -8,7 +8,7 @@ pub fn login_routes(backend: Backend) -> Router {
     Router::new()
         .route("/api/logout", post(api_logout))
         .route("/api/delete_account", post(api_delete_account))
-        .route_layer(login_required!(Backend, login_url = "/login"))
+        // .route_layer(login_required!(Backend, login_url = "/login"))
         .route("/api/login", post(api_login))
         .route("/api/register", post(api_register))
         .route("/api/auth/check", post(check_session))
