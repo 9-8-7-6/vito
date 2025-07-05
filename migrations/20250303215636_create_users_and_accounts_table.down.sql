56835c46bc1c77db7871004589e4d13be220e0b1
9-8-7-6 <ac1246897@gmail.com>
2025-07-05 23:04:27 +0800

    delete migration file for DB down

--- migrations/20250303215636_create_users_and_accounts_table.down.sql
+++
@@ -1,3 +0,0 @@
--- Add down migration script here
-DROP TABLE IF EXISTS accounts;
-DROP TABLE IF EXISTS users;
--- migrations/20250405050258_stock.down.sql
+++
@@ -1,4 +0,0 @@
--- Add down migration script here
-DROP TABLE IF EXISTS stock_metadata;
-DROP TABLE IF EXISTS stock_holdings;
-DROP TABLE IF EXISTS stock_infos;
